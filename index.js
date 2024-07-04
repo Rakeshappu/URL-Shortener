7842fe618158c8c6dc394befcca2501de6b07346
Rakeshappu <appussrh@gmail.com>
2024-07-04 19:04:49 +0530

    URL Shortener

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const express = require("express");
+const path = require("path");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url")
 const URL = require("./models/url");
-
+const staticRoute = require("./routes/staticRouter")
 
 const app = express();
 const PORT = 8001;
 
-connectToMongoDB('mongodb://localhost:27017/short-url').then(()=> console.log('mongodb connected'));
+connectToMongoDB('mongodb://localhost:27017/shorturl').then(()=> console.log('mongodb connected'));
+
+app.set("view engine", "ejs");
+app.set('views', path.resolve("./views"));
+
 
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// app.get("/test", async (req, res) => {
+//     const allUrls = await URL.find({});
+//     return res.render('home',{
+//         urls: allUrls,
+
+//     });
+// })
+
+
+
+
 
 app.use("/url", urlRoute);
+app.use("/",staticRoute)
  
-app.get('/:shortID', async (req,res)=>{
+app.get('/url/:shortID', async (req,res)=>{
     const shortID = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortID
