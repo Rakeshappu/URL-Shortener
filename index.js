5f4c2fc2bebb4c988a492c60b413cbec9097d3d2
Rakeshappu <appussrh@gmail.com>
2024-07-07 15:55:29 +0530

    URL Shortener

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 const { connectToMongoDB } = require("./connect");
-const urlRoute = require("./routes/url")
+const {restrictToLoggedinUserOnly} = require("./middlewares/auth")
 const URL = require("./models/url");
-const staticRoute = require("./routes/staticRouter")
+
+const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
 
 const app = express();
 const PORT = 8001;
@@ -16,6 +20,7 @@ app.set('views', path.resolve("./views"));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
 // app.get("/test", async (req, res) => {
 //     const allUrls = await URL.find({});
@@ -29,7 +34,8 @@ app.use(express.urlencoded({extended: false}));
 
 
 
-app.use("/url", urlRoute);
+app.use("/url",restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRoute);
 app.use("/",staticRoute)
  
 app.get('/url/:shortID', async (req,res)=>{
